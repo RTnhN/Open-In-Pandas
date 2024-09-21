5f46a5c64d8989265e0fb51e72ed0f659ef148fb
RTnhN <43109787+RTnhN@users.noreply.github.com>
2024-09-21 15:02:52 -0500

    Format with black

--- open_in_pandas.py
+++ open_in_pandas.py
@@ -2,6 +2,10 @@ import polars as pl
 import json
 import sys
 import os
+
+# Even though we don't use matplotlib here, it is nice to have it
+# for when you want to plot the data. So when you plot with
+# `df.plot()`, you can use `plt.show()` to show the plot.
 import matplotlib.pyplot as plt
 
 if len(sys.argv) != 2:
@@ -9,16 +13,16 @@ if len(sys.argv) != 2:
 else:
     file_path = sys.argv[1]
     file_ext = os.path.splitext(file_path)[1].lower()
-    
+
     try:
-        if file_ext == '.csv':
+        if file_ext == ".csv":
             df = pl.read_csv(file_path)
             print(f"CSV file '{file_path}' loaded into DataFrame 'df'.")
-        elif file_ext == '.xlsx':
+        elif file_ext == ".xlsx":
             df = pl.read_excel(file_path)
             print(f"Excel file '{file_path}' loaded into DataFrame 'df'.")
-        elif file_ext == '.json':
-            with open(file_path, 'r') as f:
+        elif file_ext == ".json":
+            with open(file_path, "r") as f:
                 data = json.load(f)
             print(f"JSON file '{file_path}' loaded into dictionary 'data'.")
         else:
